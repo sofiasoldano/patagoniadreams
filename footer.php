5af2831425274db7ca991d83e987b1de7356be2f
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2019-12-04 21:18:19 -0300

    Bug

--- footer.php
+++ footer.php
@@ -48,7 +48,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
     <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" >
+    <script type="text/javascript" src="https://cdn.rawgit.com/prashantchaudhary/ddslick/master/jquery.ddslick.min.js" ></script>
     <script src="js/vendors/masonry.pkgd.js"></script>
     <script src="js/vendors/bootstrap-multiselect.js"></script>
     <script src="js/vendors/bootstrap.min.js"></script>
